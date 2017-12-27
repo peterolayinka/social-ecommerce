e208fdf5d2cce61496aa9d31cdcbabd2474eaeb5
Peter Olayinka <peterolayinka97@gmail.com>
2017-12-28 00:23:33 +0100

    completed profile page

--- socialcommerce/account/urls.py
+++ socialcommerce/account/urls.py
@@ -1,1 +1,9 @@
-from 
+from django.urls import path
+from . import views
+
+# from store import
+app_name = 'account'
+
+urlpatterns = [
+    path('<slug:username>', views.ProfileDetailView.as_view(), name='profile_detail'),
+]
--- socialcommerce/account/views.py
+++ socialcommerce/account/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.contrib.auth import get_user_model
 
@@ -8,10 +8,11 @@ User = get_user_model()
 
 class ProfileDetailView(generic.DetailView):
     model = User
-    template_name = ''
+    template_name = 'account/profile.html'
 
-    def get_queryset(self):
-        return User.objects.get(username=self.kwargs['username'])
+    def get_object(self):
+        # import pdb; pdb.set_trace()
+        return get_object_or_404(User, username=self.kwargs.get('username'))
 
 class EditProfileView():
     pass
