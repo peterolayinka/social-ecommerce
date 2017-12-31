ac2e21644ec4d6cbf5123e04d08af18e30b2351b
Peter Olayinka <peterolayinka97@gmail.com>
2017-12-31 17:15:42 +0100

    configured static files

--- socialcommerce/socialcommerce/settings.py
+++ socialcommerce/socialcommerce/settings.py
@@ -131,6 +131,7 @@ STATICFILES_DIRS = [
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # cart session key
