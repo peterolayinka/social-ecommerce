c5b9ae41319516d7166e054b2ab5f08f3dd03a5d
Peter Olayinka <peterolayinka97@gmail.com>
2018-01-03 04:51:38 +0100

    configure heroku database

--- socialcommerce/prod_settings.py
+++ socialcommerce/prod_settings.py
@@ -100,6 +100,9 @@ DATABASES = {
     }
 }
 
+GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
