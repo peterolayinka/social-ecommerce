351592068628856db0b0a0863fc9e99f4bdc0d81
Peter Olayinka <peterolayinka97@gmail.com>
2018-01-03 04:56:31 +0100

    configure heroku database

--- socialcommerce/prod_settings.py
+++ socialcommerce/prod_settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static"),
     # '/var/www/static/',
 ]
 
